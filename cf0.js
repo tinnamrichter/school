38b7ce8f2caea23e249d1d34d9c4181be7018a15
Niels Müller Larsen <arosano@protonmail.com>
2019-08-12 17:00:20 +0200

    the naive programmers solution

---
+++ cf0.js
@@ -0,0 +1,77 @@
+'use strict';
+
+let tails = 0;
+let heads = 0;
+
+let flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+console.log('heads: ' + heads);
+console.log('tails: ' + tails);
