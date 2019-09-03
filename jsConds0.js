b23fb4d2347cfb3fb57b1d06eb0477f1214016df
tinnamrichter <tinn0229@easv365.dk>
2019-09-03 12:37:07 +0200

    conds

---
+++ jsConds0.js
@@ -0,0 +1,166 @@
+'use strict';
+
+let t1 = 0;
+let t2 = 0;
+let t3 = 0;
+let t4 = 0;
+let t5 = 0;
+let t6 = 0;
+let flip;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+	t3++;
+else if (flip === 4)
+	t4++;
+else if (flip === 5)
+	t5++;
+else
+	t6++;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+	t3++;
+else if (flip === 4)
+	t4++;
+else if (flip === 5)
+	t5++;
+else
+	t6++;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+	t3++;
+else if (flip === 4)
+	t4++;
+else if (flip === 5)
+	t5++;
+else
+	t6++;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+	t3++;
+else if (flip === 4)
+	t4++;
+else if (flip === 5)
+	t5++;
+else
+	t6++;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+	t3++;
+else if (flip === 4)
+	t4++;
+else if (flip === 5)
+	t5++;
+else
+	t6++;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+	t3++;
+else if (flip === 4)
+	t4++;
+else if (flip === 5)
+	t5++;
+else
+	t6++;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+	t3++;
+else if (flip === 4)
+	t4++;
+else if (flip === 5)
+	t5++;
+else
+	t6++;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+	t3++;
+else if (flip === 4)
+	t4++;
+else if (flip === 5)
+	t5++;
+else
+	t6++;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+	t3++;
+else if (flip === 4)
+	t4++;
+else if (flip === 5)
+	t5++;
+else
+	t6++;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+	t3++;
+else if (flip === 4)
+	t4++;
+else if (flip === 5)
+	t5++;
+else
+	t6++;
+
+console.log('t1: ' + t1);
+console.log('t2: ' + t2);
+console.log('t3: ' + t3);
+console.log('t4: ' + t4);
+console.log('t5: ' + t5);
+console.log('t6: ' + t6);
